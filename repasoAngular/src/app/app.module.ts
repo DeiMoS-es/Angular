38d5e70173737cf371eec0aa74f15be75b0782da
DeiMoS <ndelgado@hiberus.com>
2023-08-03 13:49:46 +0200

    componente product

--- repasoAngular/src/app/app.module.ts
+++ repasoAngular/src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
-import { DeimoscodeComponent } from './pages/deimoscode/deimoscode.component';
+
 import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DeimoscodeComponent
   ],
   imports: [
     BrowserModule,
