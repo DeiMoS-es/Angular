7f44f95177cefc805fcdb6283e1e69149ed1e74c
DeiMoS <ndelgado@hiberus.com>
2023-11-14 15:27:10 +0100

    creados primeros componentes

---
+++ 012-spring-autentication-JWT-Front/src/app/shared/nav/nav.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+  userLoginOn: boolean = false;//el usuario cuando inicia por primera vez no esta logueado
+  constructor(){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+}
