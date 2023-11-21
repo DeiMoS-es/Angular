f886f26ce2a86ae03b3905146dc4927b5147846b
DeiMoS <ndelgado@hiberus.com>
2023-11-21 11:17:23 +0100

    terminado registro de usuario y proyecto finalizado

--- 012-spring-autentication-JWT-Front/src/app/pages/new-user/new-user.component.ts
+++ 012-spring-autentication-JWT-Front/src/app/pages/new-user/new-user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/auth/login.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-new-user',
@@ -30,17 +31,21 @@ export class NewUserComponent implements OnInit {
     if (this.registerForm.valid) {
       // Aquí puedes hacer algo con los datos, como enviarlos al backend
       console.log('Datos del nuevo usuario:', this.registerForm.value);
-      this.loginService.registerUser(this.registerForm.value).subscribe(
+      this.loginService.registerUser(this.registerForm.value).subscribe({
         //TODO mejorar el subscribe para usarlo de forma actual
-        (Response) => {
-          console.log("Exito");
-        },
-        (error)=>{
-          console.log("Error");
+        next: () => {},
+        error: (e) => console.error('Ha ocurrido un error: ', e),
+        complete: () => {
+          Swal.fire({
+            text: 'Logeado con éxito',
+            icon: 'success',
+            timer: 1000, // Tiempo en milisegundos (en este caso, 1 segundos)
+            showConfirmButton: false, // Ocultar el botón de confirmación
+          });
+          this.router.navigateByUrl('/iniciar-sesion');
+          this.limpiarFormulario();
         }
-      );
-      // Redirigir a la página de inicio después de registrar el usuario
-      this.router.navigateByUrl('/inicio');
+    });
     }
   }
 
