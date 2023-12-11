04a26dcc7f774d3fb479a2192acb75d506891cbf
DeiMoS <ndelgado@hiberus.com>
2023-12-11 09:20:47 +0100

    añadido mensaje de confirmación guardar producto y mensajes del tipo de archivo seleccionado

--- 013-spring-apirest-front/src/app/pages/guardar-producto/guardar-producto.component.ts
+++ 013-spring-apirest-front/src/app/pages/guardar-producto/guardar-producto.component.ts
@@ -26,15 +26,46 @@ export class GuardarProductoComponent implements OnInit{
       imagenProducto: [null] // Inicializado con null ya que no es requerido inicialmente
     })
   };
-  public onFileSelected(event: any) {
-    const file = event.target.files[0];
-    console.log(file);
-    // Puedes realizar validaciones adicionales aquí si es necesario
-    // Asigna el archivo seleccionado al campo imagenProducto del formulario
-    this.productoForm.patchValue({
+  public onFileSelected(event: any):void {
+    const file: File | null = event.target.files?.[0] || null;
+
+    if (file) {
+      // Verificar el tamaño máximo del archivo (ejemplo: 10 MB)
+      const maxSizeInBytes = 10 * 1024 * 1024; // 10 MB
+      if (file.size > maxSizeInBytes) {
+        Swal.fire({
+          title: "Error: El archivo es demasiado grande. Tamaño máximo permitido: 10 MB.",
+          icon: "error",
+          timer: 2000, // Tiempo en milisegundos (en este caso, 3 segundos)
+          showConfirmButton: false, // Ocultar el botón de confirmación
+        })
+        console.error('Error: El archivo es demasiado grande. Tamaño máximo permitido: 10 MB.');        
+        return;
+      }
+      // Verificar el tipo de archivo (ejemplo: solo imágenes permitidas)
+      const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+      if (!allowedTypes.includes(file.type)) {
+        Swal.fire({
+          title: "Error: El archivo no es una imagen válido. Solo se permiten archivos JPEG, PNG o GIF.",
+          icon: "error",
+          timer: 2000, // Tiempo en milisegundos (en este caso, 3 segundos)
+          showConfirmButton: false, // Ocultar el botón de confirmación
+        })
+        console.error('Error: El archivo no es una imagen válido. Solo se permiten archivos JPEG, PNG o GIF.');        
+        return;
+      }
+      // Asignar el archivo seleccionado al campo imagenProducto del formulario
+      this.productoForm.patchValue({
         imagenProducto: file
-    });
+      });
+      // Si es necesario, puedes mostrar la vista previa de la imagen aquí
+      // this.mostrarVistaPrevia(file);
+    } else {
+      console.error('Error: No se seleccionó ningún archivo.');
+      // Puedes agregar un mensaje al usuario o realizar otras acciones según tus necesidades
+    }
   }
+
 
   public onSubmit(){
     this.formularioEnviado = true;
@@ -56,45 +87,27 @@ export class GuardarProductoComponent implements OnInit{
       },
       error: (err) => {
         console.log("Error: ", err);
+        Swal.fire({
+            title: "Error al crear el producto",
+            icon: "error",
+            timer: 2000, // Tiempo en milisegundos (en este caso, 3 segundos)
+            showConfirmButton: false, // Ocultar el botón de confirmación
+        })
       },
       complete: () => {
-        console.log("Complete");
+        Swal.fire({
+            title: "Producto creado",
+            icon: "success",
+            timer: 2000, // Tiempo en milisegundos (en este caso, 3 segundos)
+            showConfirmButton: false, // Ocultar el botón de confirmación
+        });
+          this.route.navigate(['dashboard']);
       }
     });
   } else{
         this.productoForm.markAllAsTouched();
       }
   }
-  // public onSubmit(){
-  //   this.formularioEnviado = true;
-  //   if(this.productoForm.valid){
-  //     console.log(this.productoForm.value);
-  //     this.productoService.guardarProducto(this.productoForm.value).subscribe({
-  //       next:(data) =>{console.log(data);},
-  //       error:(err) =>{
-  //         console.log("Error: ", err);
-  //         Swal.fire({
-  //           title: "Error al crear el producto",
-  //           icon: "error",
-  //           timer: 2000, // Tiempo en milisegundos (en este caso, 3 segundos)
-  //           showConfirmButton: false, // Ocultar el botón de confirmación
-  //         })
-  //       },
-  //       complete:() =>{
-  //         console.log("Complete");
-  //         Swal.fire({
-  //           title: "Producto creado",
-  //           icon: "success",
-  //           timer: 2000, // Tiempo en milisegundos (en este caso, 3 segundos)
-  //           showConfirmButton: false, // Ocultar el botón de confirmación
-  //         });
-  //         this.route.navigate(['dashboard']);
-  //       }
-  //     })
-  //   } else{
-  //     this.productoForm.markAllAsTouched();
-  //   }
-  // };
 
   public limpiarFormulario() {
     this.productoForm.reset(); // Restablecer el formulario
