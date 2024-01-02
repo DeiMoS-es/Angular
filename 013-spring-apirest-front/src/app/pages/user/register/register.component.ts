f9faf77e2e6e9ab5ca42ffe531d42d0b42e48cef
DeiMoS <ndelgado@hiberus.com>
2024-01-02 17:47:43 +0100

    creado formulario de registro de usuarios

--- 013-spring-apirest-front/src/app/pages/user/register/register.component.ts
+++ 013-spring-apirest-front/src/app/pages/user/register/register.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  // Variables
+  userForm: FormGroup
+  formularioEnviado = false;
+
+  constructor(private usuarioService: UsuarioService, private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    
+  }
+
+  public onSubmit(): void{
+
+  }
+
+  public limpiarFormulario(): void {
+    
+  }
 
 }
